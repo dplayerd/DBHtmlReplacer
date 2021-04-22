f219afbf7808b46104e07f74d59ddf73d7afccef
Moudou <dplayerd@gmail.com>
2021-04-22 08:18:25 +0800

    [Dev] - Add input controls

--- DBHtmlReplacer/GUI/Form1.Designer.cs
+++ DBHtmlReplacer/GUI/Form1.Designer.cs
@@ -32,28 +32,39 @@ namespace GUI
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.btnReplace = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TableName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PKName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(24, 148);
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 349);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(552, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(811, 96);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "<a href=\"aaa\" target=\"_blank\">GOGOGO</a><br/>\n<a href=\"aaa\">GOGOGO</a><br/>\n<a hr" +
     "ef=\"aaa\" target=\"_blank\">GOGOGO</a>";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(24, 269);
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 470);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(552, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(811, 96);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
             // btnReplace
             // 
-            this.btnReplace.Location = new System.Drawing.Point(602, 342);
+            this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReplace.Location = new System.Drawing.Point(870, 543);
             this.btnReplace.Name = "btnReplace";
             this.btnReplace.Size = new System.Drawing.Size(75, 23);
             this.btnReplace.TabIndex = 2;
@@ -61,16 +72,67 @@ namespace GUI
             this.btnReplace.UseVisualStyleBackColor = true;
             this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TableName,
+            this.ColumnName,
+            this.PKName});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 124);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(933, 172);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // TableName
+            // 
+            this.TableName.HeaderText = "表名";
+            this.TableName.MinimumWidth = 10;
+            this.TableName.Name = "TableName";
+            this.TableName.Width = 150;
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.HeaderText = "欄名";
+            this.ColumnName.MinimumWidth = 10;
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.Width = 150;
+            // 
+            // PKName
+            // 
+            this.PKName.HeaderText = "PK";
+            this.PKName.MinimumWidth = 10;
+            this.PKName.Name = "PKName";
+            this.PKName.Width = 150;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.Location = new System.Drawing.Point(12, 33);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(933, 61);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 579);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnReplace);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -80,6 +142,11 @@ namespace GUI
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TableName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PKName;
+        private System.Windows.Forms.RichTextBox richTextBox3;
     }
 }
 
